6f3f1a9a9dbe05857283cacc711ed5d37444b283
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-11-10 11:05:34 -0600

    HTML / CSS Modifications: - Building out the wireframe, testing the form - decided to make this a one page site, deleted contacts and portfolio - went away from bootstrap v4, went back to v3. - added more images - I have a basic framework, it is a start, I want to modify it more. - Moved css to external-css - Created CSS animations for when the page initially loads. looks ok, needs some work.
    JavaScript/Jquery Modifications:
    - created external js for custom js
    - I am using the reveal.js library to build animations and reveal content as the user scrolls down the page
    - I downloaded packery to use their custom grid system.
    
    README.md modifications:
    - None
    
    TODO:
    - Add Github api / integrate my projects into site
    - Add content to the site
    - Styling needs a lot of work, the form looks bad.

---
+++ js/hero.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var $grid = $('.grid').imagesLoaded( function() {
+    // init Packery after all images have loaded
+    $grid.packery({
+        itemSelector: '.grid-item',
+        percentPosition: true
+    });
+});
+
